d8823991cf2d676dea4ef906f026d22a7850b47f
Austin <austin.amaral3@gmail.com>
2023-10-21 12:10:50 -0700

    two post done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,12 +1,35 @@
 const express = require('express');
-
+const Post = require('./posts-model.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json('foo')
+    Post.find()
+      .then(posts => {
+          res.status(200).json(posts);
+      })
+      .catch(error => {
+          // console.log(error);
+          res.status(500).json({message: 'The posts information could not be retrieved',
+          error: error.message,
+          stack: error.stack,
+        });
+      })
 })
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({message: 'The post with the specified ID does not exist'})
+        } else {
+            res.json(post)
+        }
+    }  catch (error) {
+            // console.log(error);
+            res.status(500).json({message: 'The posts information could not be retrieved',
+            error: error.message,
+            stack: error.stack,
+          });
+        }
 })
 router.post('/', (req, res) => {
 
